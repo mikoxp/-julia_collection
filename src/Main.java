7c32cd867d94b3a4878460d92b66ec4c0b7a53a8
moles <olesmichalxp@gmail.com>
2017-04-04 20:24:08 +0200

    start:

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int size=1000;
+        File file=new File("test.jpg");
+        BufferedImage bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+        try {
+            ImageIO.write(bufferedImage, "jpg", file);
+        }catch (IOException e){
+            System.err.println(e);
+        }
     }
 }
